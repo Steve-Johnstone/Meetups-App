06c24cb7d80ccb806952a733637a841bdca3ec9c
Steve Johnstone <t-sjohnstone@hotels.com>
2021-10-29 11:39:40 +0100

    first commit

---
+++ meetups-app/src/App.js
@@ -0,0 +1,26 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetUpsPage from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favourites";
+import MainNavigation from "./components/layout/MainNavigation";
+
+function App() {
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetUpsPage />
+        </Route>
+        <Route path="/favourites">
+          <FavouritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
